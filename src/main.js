22ce607180dac97d71f805b515023002ae1c50a2
Orz3ch <konrad.orzeszek@gmail.com>
2022-12-17 10:50:13 +0100

    TASK-3: 404 not found page & router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BaseCard from ".//ui/BaseCard.vue";
 
 createApp(App).use(router).mount("#app");
+
+App.component("base-card", BaseCard);
